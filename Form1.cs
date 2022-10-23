4557a4a4196d9c970f51127db72bb7ba0cf3cef4
0BaraACuda0 <62384713+Artemenyh@users.noreply.github.com>
2022-10-23 23:21:00 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        logik logik = new logik();  
+        float a, b;
+        int count;
+        bool znak = true;
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 0;
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+
+            logik.Tochka(textBox1);
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 1;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 2;
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 3;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 4;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 5;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 6;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 7;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 8;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 9;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            logik.ZnakChisla(textBox1);
+            
+        }
+        
+        
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            logik.Operation(textBox1,1,label1);
+            a = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            count = 1;
+            label1.Text = a.ToString() + "+";
+            znak = true;
+
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            logik.Operation(textBox1, 2, label1);
+            a = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            count = 2;
+            label1.Text = a.ToString() + "-";
+            znak = true;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            logik.Operation(textBox1, 3, label1);
+            a = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            count = 3;
+            label1.Text = a.ToString() + "*";
+            znak = true;
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            logik.Operation(textBox1, 4, label1);
+            a = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            count = 4;
+            label1.Text = a.ToString() + "/";
+            znak = true;
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            logik.Solushion(textBox1, label1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            logik.Clear(textBox1, label1);
+        } 
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == ',')
+{
+                if (textBox1.Text.IndexOf(',') != -1)
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int lenght = textBox1.Text.Length - 1;
+            string text = textBox1.Text;
+            textBox1.Clear();
+            for (int i = 0; i < lenght; i++)
+            {
+                textBox1.Text = textBox1.Text + text[i];
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ logik.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace Calculator
+{
+    public class logik
+    {
+        string a ="", b="", c="";
+        int Num = 0;
+        int count;
+        int opt = -1;
+        bool minus = false;
+        bool minus2 = false;
+        bool znak = true;
+        public void reading(string num, TextBox textBox1)
+        {
+            if (Num == 0 && a.Length < 9)
+            {
+                a += num;
+                textBox1.Text = a;
+            }
+            if (Num == 1 && b.Length < 9)
+            {
+                b += num;
+                textBox1.Text = b;
+            }
+        }
+        public void ZnakChisla(TextBox textBox1)
+        {
+            if (znak == true)
+            {
+                textBox1.Text = "-" + textBox1.Text;
+                znak = false;
+            }
+            else if (znak == false)
+            {
+                textBox1.Text = textBox1.Text.Replace("-", "");
+                znak = true;
+            }
+        }
+        public void Tochka(TextBox textBox1)
+        {
+            if (textBox1.Text.IndexOf(',') == -1)
+            {
+                textBox1.Text += ",";
+            }
+        }
+        public void Solushion(TextBox textBox1, Label label1)
+        {
+            double sum = 0;
+            {
+                if ((a != "0" && b != "0") || opt != 3)
+                {
+                    try
+                    {
+                        if (minus)
+                        {
+                            int buf;
+                            buf = -1 * Convert.ToInt32(a);
+                            a = Convert.ToString(buf);
+                        }
+                        if (minus2)
+                        {
+                            int buf;
+                            buf = -1 * Convert.ToInt32(b);
+                            b = Convert.ToString(buf);
+                        }
+                        switch (opt)
+                        {
+                            case 1:
+                                sum = Convert.ToDouble(a) + float.Parse(textBox1.Text);
+                                textBox1.Text = b.ToString();
+                                break;
+
+                            case 2:
+                                sum = Convert.ToDouble(a) - float.Parse(textBox1.Text);
+                                textBox1.Text = b.ToString();
+                                break;
+                            case 3:
+                                sum = Convert.ToDouble(a) * float.Parse(textBox1.Text);
+                                textBox1.Text = b.ToString();
+                                break;
+                            case 4:
+                                sum = Convert.ToDouble(a) / float.Parse(textBox1.Text);
+                                textBox1.Text = b.ToString();
+                                break;
+
+                            default:
+                                break;
+                        }
+                    
+                        if (sum <= 999999999 && sum >= -999999999)
+                        {
+                            if (sum < 0)
+                            {
+                                label1.Text = "-";
+                            }
+                            c = Convert.ToString(sum);
+                            c = c.Replace("-", "");
+                            
+                            textBox1.Text = c;
+                        }
+                        else
+                        {
+                            textBox1.Text = "EXCEEDED";
+                        }
+                        a = c;
+                        b = "";
+                        c = "";
+                        Num = 0;
+                    }
+                    catch (Exception)
+                    {
+                        //MessageBox.Show("Error, check the data");
+                    }
+                }
+                else
+                {
+                    textBox1.Text = "NOT / 0";
+                    a = "";
+                    b = "";
+                    c = "";
+                    Num = 0;
+                }
+            }
+        }        public void ClearSimvol(TextBox textBox1)
+        {
+            int lenght = textBox1.Text.Length - 1;
+            string text = textBox1.Text;
+            textBox1.Clear();
+            for (int i = 0; i < lenght; i++)
+            {
+                textBox1.Text = textBox1.Text + text[i];
+            }
+        }
+        //public void Clear(TextBox textBox1, Label label1)
+        //{
+        //    textBox1.Text = "";
+        //    label1.Text = "";
+        //}
+        
+        public void Operation(TextBox textBox1, int option, Label label1)
+        {
+            if (a != "")
+            {
+                if (b != "")
+                {
+                    Solushion(textBox1, label1);
+                }
+                Num = 1;
+                label1.Text = "";
+            }
+            opt = option;
+
+        }
+         public void Clear(TextBox textBox1, Label label1)
+        {
+            textBox1.Text = c;
+            a = "";
+            b = "";
+            c = "";
+            textBox1.Text = "";
+            Num = 0;
+            label1.Text = "";
+            minus = false;
+            minus2 = false;
+        }
+        public void ReverseMinus(Label label1)
+        {
+            if (Num == 0)
+            {
+                if (label1.Text == "")
+                {
+                    label1.Text = "-";
+                    minus = true;
+                }
+                else
+                {
+                    label1.Text = "";
+                    minus = false;
+                }
+            }
+            if (Num == 1)
+            {
+                if (label1.Text == "")
+                {
+                    label1.Text = "-";
+                    minus2 = true;
+                }
+                else
+                {
+                    label1.Text = "";
+                    minus2 = false;
+                }
+            }
+        }
+    
+}
+     
+}
